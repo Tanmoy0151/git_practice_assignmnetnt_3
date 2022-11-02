0c44515908c38ab7f7a7baa42c270d7f56b927f0
Tanmoy0151 <royanupama782@gmail.com>
2022-11-02 23:59:31 +0530

    I have done

---
+++ prime_number.js
@@ -0,0 +1,22 @@
+function PrimeNumber(Number){
+    let Factor=0;
+    for(i=1;i<=Number;i++){
+        if(Number%i==0){
+            Factor++;
+        }
+    }
+d
+
+    if(Factor==2){
+        return true;
+    }else{
+        return false;
+    }
+}
+let userInput=3274
+let answer=PrimeNumber(userInput);
+if(answer==true){
+    console.log("Prime number");
+}else{
+    console.log("Not a prime number");
+}
