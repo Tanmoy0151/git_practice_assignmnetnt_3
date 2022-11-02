605a5a3c9b12f7aef66b8daf9d695e992c506e23
Tanmoy0151 <royanupama782@gmail.com>
2022-11-02 22:35:41 +0530

    vai apna kam karo

---
+++ primeNumber.js
@@ -0,0 +1,13 @@
+let num=prompt( );
+let count=0;
+for(let i=1;i<=num;i++){
+   
+    }
+}if(count==2){
+    console.log("This is a Prime  Number");
+}else{
+    console.log("This is not a Prime  Number");
+}
+//please currect it .All of you
+
+
